f6d4688c06cd90b5a239dac04e693945bc7c097a
Viktor Kocur <kocurvik@gmail.com>
2024-11-20 11:44:14 +0100

    run just p3p for calib case

--- eval.py
+++ eval.py
@@ -134,10 +134,12 @@ def print_results(experiments, results, eq_only=False):
 
 def eval(args):
 
-    experiments = [f'3p_monodepth+{i}' for i in range(1, 13)]
-    experiments.extend([f'3p_reldepth+{i}' for i in range(1, 13)])
-    experiments.extend([f'p3p+{i}' for i in range(1, 13)])
-    experiments.append('5p')
+    # experiments = [f'3p_monodepth+{i}' for i in range(1, 13)]
+    # experiments.extend([f'3p_reldepth+{i}' for i in range(1, 13)])
+    # experiments.extend([f'p3p+{i}' for i in range(1, 13)])
+    # experiments.append('5p')
+
+    experiments = [f'p3p+{i}' for i in range(1, 13)]
 
 
     # experiments = ['5p_nister', '3dp_monodepth+moge', '3dp_monodepth+marigold-bm', '3dp_reldepth+moge', '3dp_reldepth+marigold-bm']
